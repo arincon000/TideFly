f9dba6fb76725d375755341a765ceb9a07d3662d
arincon000 <77942403+arincon000@users.noreply.github.com>
2025-08-15 16:30:01 -0500

    Create days.ts

---
+++ vercel-app/lib/days.ts
@@ -0,0 +1,3 @@
+export const MON = 1 << 0, TUE = 1 << 1, WED = 1 << 2, THU = 1 << 3, FRI = 1 << 4, SAT = 1 << 5, SUN = 1 << 6;
+export const WEEKDAYS = MON | TUE | WED | THU | FRI;
+export const WEEKENDS = SAT | SUN; // 0b1100000 = 96
