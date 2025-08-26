5be61057023ab051b55901259ac74e4b32a71f5f
arincon000 <77942403+arincon000@users.noreply.github.com>
2025-08-26 10:52:30 -0500

    Update and rename tailwind.config.mjs to tailwind.config.js

---
+++ vercel-app/tailwind.config.js
@@ -0,0 +1,6 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,js,jsx,mdx}", "./components/**/*.{ts,tsx,js,jsx,mdx}"],
+  theme: { extend: {} },
+  plugins: [],
+};
